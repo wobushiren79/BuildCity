5f7a92398ec4953268562fc123e8488630338f50
Joker Apple <734719526@qq.com>
2021-01-12 15:29:33 +0800

    框架创建

---
+++ BuildCity/Assets/Editor/AssetBundles/AssetBundlesEditor.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class AssetBundlesEditor : Editor
+{
+
+    [MenuItem("Custom/AssetBundles/BuildAssetBundle")]
+    public static void BuildAssetBundle()
+    {
+
+        //根据BuildSetting里面所激活的平台进行打包 设置过AssetBundleName的都会进行打包
+        string dir = "Assets/StreamingAssets";
+        if (Directory.Exists(dir) == false)
+        {
+            Directory.CreateDirectory(dir);
+        }
+        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.UncompressedAssetBundle, EditorUserBuildSettings.activeBuildTarget);
+        AssetDatabase.Refresh();
+        Debug.Log("打包完成");
+
+    }
+
+
+    /// <summary>
+    /// 清除之前设置过的AssetBundleName，避免产生不必要的资源也打包
+    /// 之前说过，只要设置了AssetBundleName的，都会进行打包，不论在什么目录下
+    /// </summary>
+    [MenuItem("Custom/AssetBundles/ClearAssetBundlesName")]
+    public static void ClearAssetBundlesName()
+    {
+        int length = AssetDatabase.GetAllAssetBundleNames().Length;
+        Debug.Log(length);
+        string[] oldAssetBundleNames = new string[length];
+        for (int i = 0; i < length; i++)
+        {
+            oldAssetBundleNames[i] = AssetDatabase.GetAllAssetBundleNames()[i];
+        }
+
+        for (int j = 0; j < oldAssetBundleNames.Length; j++)
+        {
+            AssetDatabase.RemoveAssetBundleName(oldAssetBundleNames[j], true);
+        }
+        length = AssetDatabase.GetAllAssetBundleNames().Length;
+        Debug.Log(length);
+    }
+}
